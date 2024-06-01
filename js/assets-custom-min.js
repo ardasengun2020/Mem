b3511a42d3c97c68d157890d89942019eb8eddfb
ardasengun2020 <53059950+ardasengun2020@users.noreply.github.com>
2024-06-01 13:59:44 -0400

    Add files via upload

---
+++ js/assets-custom-min.js
@@ -0,0 +1,1 @@
+!function(t){"use strict";jQuery(".open-product-hint-pop-up").magnificPopup({type:"inline",midClick:!0,removalDelay:500,callbacks:{beforeOpen:function(){this.st.mainClass=this.st.el.attr("data-effect")}}})}();
